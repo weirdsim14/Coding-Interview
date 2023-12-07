1359c672c537680054d970e1c0f1a78b8124b6cc
sim <sim30217@gmail.com>
2023-12-07 19:22:03 +0900

    braces

---
+++ braces.cpp
@@ -0,0 +1,28 @@
+#include <stack>
+#include <string>
+
+bool bracesMatching(const std::string& bracesStr) {
+    std::stack<char> stackBraces;
+
+    int len = bracesStr.length();
+
+    for (int i = 0; i < len; i++) {
+        switch (bracesStr[i]) {
+            case '{':
+                stackBraces.push(bracesStr[i]);
+                break;
+
+            case '}':
+                if (stackBraces.empty()) {
+                    return false;
+                }
+                stackBraces.pop();
+                break;
+
+            default:
+                return false;
+        }
+    }
+
+    return stackBraces.empty();
+}
